8b9a051cee1a769f916239da4f0ac10fdb76d8bf
samaksh2434 <1000019969@dit.edu.in>
2025-07-06 18:28:23 +0530

    here i have made movement of the body and the head of the snake

--- gamepanel.java
+++ gamepanel.java
@@ -10,15 +10,36 @@ import java.awt.event.KeyListener;
 public class gamepanel extends JPanel implements KeyListener,ActionListener 
 
 {
+    public int[] xpos=new int[999999];
+    public int[] ypos=new int[999999];
+    { xpos[0]=100;
+    ypos[0]=100;}
     
-
+    
+    public int snakelenght=9;
+    int ti=snakelenght;    
+    final int UP = 0;
+    final int DOWN = 1;
+    final int LEFT = 2;
+    final int RIGHT = 3;
+    int direction = DOWN;
+     // initial direction
+    
     public gamepanel(){
+        
         this.addKeyListener(this);
         this.setFocusable(true);
         
     }
 
+
+  
+
+    
+
+
     public void paint(Graphics g){
+         
         
         //titele    bar
         g.setColor(Color.blue);
@@ -38,41 +59,54 @@ public class gamepanel extends JPanel implements KeyListener,ActionListener
         g.drawRect(20, 90, 745, 455);
         g.fillRect(23, 93, 740, 450);
 
-        //snake body 
-        // Image image;
-        // image = new ImageIcon("C:\\Users\\RealS\\Documents\\GitHub\\Snakegame\\Screenshot 2025-07-05 172554.png").getImage();
-        // g.drawImage(image, 100, 100, 20, 20, null);
-        snakelenght(g);
         
 
-
+        //snakes body is here with the snake body arrray named slenght a= SNAKE LENGHT
         
         
-
+        int[] slength=new int[snakelenght];
         
-    }
- // here this will print the snake based on its size also this fuction will keep the track of the
- //snake lenght and size
-    public void snakelenght(Graphics g){
-        int a=3;
-        int[] slength=new int[a];
+        //this loop will assingn the number in the slenth array
+        for(int i=0;i<slength.length;i++){
+            slength[i]=i;
+            
+        }
+        
+        
+        //this loop will print the snake
         for(int i=0;i<slength.length;i++){
 
             if(slength[i]==0){
                 Image image;
                 image = new ImageIcon("C:\\Users\\RealS\\Documents\\GitHub\\Snakegame\\Screenshot 2025-07-05 172554.png").getImage();
-                g.drawImage(image, 100, 100, 20, 20, null);
+                g.drawImage(image, xpos[0], ypos[0], 20, 20, null);
+                //  System.out.println(xpos[0]+" "+ypos[0]);
             }
-            if(i>0){
+            
+
+            if(slength[i]>0){
+                
                 Image image;
                 image = new ImageIcon("C:\\Users\\RealS\\Documents\\GitHub\\Snakegame\\Screenshot 2025-07-05 172452.png").getImage();
-                g.drawImage(image, 100-(i*30), 100, 20, 20, null);
+                g.drawImage(image, xpos[ti-i], ypos[ti-i], 20, 20, null);
+                // System.out.println(xpos[ti-i]+" "+ypos[ti-i]);
             }
+           
 
             
         }
 
+        
+
+
+        
+        
+
+        
     }
+    
+
+    //THIS IS THE INPUT AREA HERE THE USER INPUTS ARE TAKEN INTO ACCOUNT
 
  @Override
     public void actionPerformed(ActionEvent e) {
@@ -81,24 +115,74 @@ public class gamepanel extends JPanel implements KeyListener,ActionListener
     }
     @Override
     public void keyPressed(KeyEvent e) {
+            
+           
+            xpos[ti]=xpos[0];
+            ypos[ti]=ypos[0];
+
+            System.out.println(direction);
         
         if(e.getKeyCode()==KeyEvent.VK_RIGHT){
-            System.out.println("right");
+            if(direction!= LEFT){
+                direction=RIGHT;
+                System.out.println("right");
+            xpos[0]=xpos[0]+20;
+            repaint();
+             ti++;
+            }
+            
+             
+            
+            
+        
+            
 
         }
         if(e.getKeyCode()==KeyEvent.VK_LEFT){
-            System.out.println("left");
+            if(direction!= RIGHT){
+                direction=LEFT;
+                System.out.println("left");
+            xpos[0]=xpos[0]-20;
+            repaint();
+             ti++;
+            }
+             
+            
         }
         if(e.getKeyCode()==KeyEvent.VK_UP){
-            System.out.println("up");
+            
+            if(direction!= DOWN){
+                direction=UP;
+                System.out.println("up");
+            ypos[0]=ypos[0]-20;
+            repaint();
+             ti++;
+            }
+             
+             
+
+            
         }
+
         if(e.getKeyCode()==KeyEvent.VK_DOWN){
-            System.out.println("down");
+            if(direction!=UP){
+                direction=DOWN;
+                System.out.println("down");
+            ypos[0]=ypos[0]+20;
+            repaint();
+             ti++;
+            }
         }
+    }  
+             
+            
+        
+
+
 
         
         
-    }
+    
 
 
 
